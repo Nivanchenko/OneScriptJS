52fa1bce19ab67960f9ffeb49815428e49049f56
Nikita Ivanchenko <n1k17ka@gmail.com>
2025-11-13 14:27:57 +0300

    wasm build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,2 +1,5 @@
-tree-sitter generate
-tree-sitter build-wasm --output wasm/tree-sitter-onescript.wasm
+mv package.json _package.json
+rm -rf build/wasm | true
+tree-sitter generate --output build/wasm/src
+mv _package.json package.json
+tree-sitter build build/wasm --output wasm/tree-sitter-onescript.wasm --wasm
