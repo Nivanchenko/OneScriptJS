36cf2387bb113a567171c5463a42d868a59916f4
Nikita Ivanchenko <n1k17ka@gmail.com>
2026-03-03 12:03:42 +0300

    keep debug

--- debug.ts
+++ debug.ts
@@ -16,21 +16,26 @@ function walk(node: Node, depth = 0) {
   const OS = new OneScript('./wasm/tree-sitter-onescript.wasm');
 
   const source = 
+  // `
+  //   а = 1;
+  //   Если а = 2 Тогда
+  //     б = 2;
+  //   КонецЕсли;
+  // `
+   `
+    а = 1;
+    б = 0;
+    Для н = 1 по 10 Цикл
+        а = а + н;
+        Прервать;
+        б = 1;
+    КонецЦикла;
   `
-  а = 3;
-  в = 0;
-  Если а = 1 Тогда
-      в = 1;
-  ИначеЕсли а = 2 Тогда
-      в = 2;
-  Иначе
-      в = 3;
-  КонецЕсли;
-  `;
+  ;
    
-  const tree = await OS.GetTree(source);
-  if (tree) walk(tree.rootNode);
-  console.log(tree?.rootNode.toString());
+  // const tree = await OS.GetTree(source);
+  // if (tree) walk(tree.rootNode);
+  // console.log(tree?.rootNode.toString());
   await OS.Run(source);
   const variables = OS.dumpVariables();
   console.log(variables);
