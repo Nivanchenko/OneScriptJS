60284ebd0e7ef02fb10e97792e9dac1522390d06
Ilya Bushin <big@mail.ru>
2026-04-05 00:58:05 +0300

    Не хватает экспортов

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 export { OneScript } from './OneScript.js';
+export { OSCompiler } from './OneScriptCompiler.js';
+export { OSparser } from './OneScriptParser.js';
+export { OSMachine } from './OneScriptVirtualMachine.js';
